af61ff4c3e8fd2f9ba393af9a913468356f215f4
Shunya Seiya <shunchan0677@gmail.com>
2023-07-08 07:57:27 +0900

    Update command

--- run_interp.sh
+++ run_interp.sh
@@ -1,18 +1,18 @@
 export PYTHONPATH=$PYTHONPATH:../Downloads/carla_0.9.6/PythonAPI/carla/dist/carla-0.9.6-py3.5-linux-x86_64.egg
 
-#CUDA_VISIBLE_DEVICES=0 python3 run.py train_policy_with_original_rewards carla-v0 --n_envs 1 --million_timesteps 1 \
-#  --root_dir logs \
-#  --experiment_name latent_sac \
-#  --gin_file params.gin \
-#  --gin_param load_carla_env.port=2000
-
-
-
-CUDA_VISIBLE_DEVICES=0 python3 run.py train_policy_with_preferences carla-v0 --n_envs 1 --million_timesteps 1 --synthetic_prefs \
+CUDA_VISIBLE_DEVICES=0 python3 run.py train_policy_with_original_rewards carla-v0 --n_envs 1 --million_timesteps 1 \
   --root_dir logs \
   --experiment_name latent_sac \
   --gin_file params.gin \
   --gin_param load_carla_env.port=2000
+
+
+
+#CUDA_VISIBLE_DEVICES=0 python3 run.py train_policy_with_preferences carla-v0 --n_envs 1 --million_timesteps 1 --synthetic_prefs \
+#  --root_dir logs \
+#  --experiment_name latent_sac \
+#  --gin_file params.gin \
+#  --gin_param load_carla_env.port=2000
 
 
 #CUDA_VISIBLE_DEVICES=0 python3 run.py train_policy_with_preferences carla-v0 --n_envs 1 --million_timesteps 1 \
@@ -42,3 +42,12 @@ CUDA_VISIBLE_DEVICES=0 python3 run.py train_policy_with_preferences carla-v0 --n
 #  --experiment_name latent_sac \
 #  --gin_file params.gin \
 #  --gin_param load_carla_env.port=2000 
+
+
+
+#CUDA_VISIBLE_DEVICES=0 python3 run.py train_policy_with_preferences carla-v0 --n_envs 1 --million_timesteps 1  \
+#  --root_dir logs \
+#  --experiment_name latent_sac \
+#  --gin_file params.gin \
+#  --gin_param load_carla_env.port=2000 \
+#  --load_prefs_dir=runs/1688600764_e2f5ab2
